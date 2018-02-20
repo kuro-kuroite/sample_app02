1320c67f1388a9c94cf5aff4cd6e987549607b57
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-02-20 02:34:50 +0000

    create user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -6,4 +15,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
