95727d20868caa7293cb69c2689002121e754089
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-02-20 07:45:00 +0000

    improve user login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   get  '/signup',  to: 'users#new'
 
   get  '/help',    to: 'static_pages#help'
