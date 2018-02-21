552a0b7ab064d3561dd901f20a83c50d0eb2cd03
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-02-21 09:38:15 +0000

    redirect root_path if user does not login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -38,4 +40,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
 end
